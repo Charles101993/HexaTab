9d716a7de28f70807dc7a075e6d68904bb91a9f6
charles101993 <charles101993@tamu.edu>
2016-11-27 16:20:29 -0600

    Update test.php

--- test.php
+++ test.php
@@ -3,18 +3,36 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+	<script src= "xml_req_.js"></script>
+	
 	
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>jQuery UI Draggable - Default functionality</title>
+	<title>HexaTab</title>
 
 	<link rel="stylesheet" href="css/style.css">
 	
 	<style>
+	header {
+	height:50px;
+	background:#F0F0F0;
+	border:1px solid #CCC;
+	width:960px;
+	margin:0px auto
+	}
+	h1 {
+	font-family:georgia, serif;
+	color:#786E69;
+	font-size:25px;
+	font-weight:bold;
+	letter-spacing:.1em;
+	text-transform:uppercase;
+	padding-bottom:3px;
+	}
+	
 	body
 	{
-		background-color: #000000;
-		background-image: /assets/web.jpg
+		background-image: url("assets/web.jpg");
 	}
 	#draggable { height: 60px; width: 60px; ; padding: 0.5em; background:transparent; border:transparent;}
 	
@@ -42,10 +60,10 @@
 		display:none;
 	}
 	
+		
 	img {
-    -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
-    filter: grayscale(100%);
-
+	    -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+	    filter: grayscale(100%);
 	}
 	
 	</style>
@@ -112,6 +130,21 @@
 	} );
 	</script>
 </head>
+<h1>
+
+<div id = "menuText" style="position: absolute; left: 300px; top: 90px;" onclick = "disDrop(menuDrop);"> 
+Sign Out
+</div>
+<div id = "newTabText"  style="position: absolute; left: 500px; top: 90px;" onclick ="duplicate();useID();"> 
+New Flake	
+</div>
+<div id = "saveButtonText" style="position: absolute; left: 720px; top: 90px;" onclick: "save();"> 
+Save
+</div>
+<div id = "deleteButtonText" style="position: absolute; left: 830px; top: 90px;" onclick: "toggleDelete();"> 
+Delete
+</div>
+</h1>
 
 <div id = "menuButton" style="position: absolute; left: 10px; top: 10px;"> 
     <img src='assets/menu.png' width="50" height="50" onclick = "disDrop(menuDrop);"/>
@@ -136,7 +169,7 @@
 </div>
 
 <div id = "saveButton" style="position: absolute; left: 150px; top: 10px;"> 
-    <img src='assets/save.png' onclick= "save()"; width="50" height="50"/>
+    <img src='assets/save.png' onclick= "save();" width="50" height="50"/>
 </div>
 
 <div id = "editModeButton" style="position: absolute; left: 275px; top: 10px;"> 
@@ -159,19 +192,10 @@
 	<img id = "deleteImg2" src='assets/deleteActive.png' width="50" height="50" style="display:none" onclick = "toggleDelete();"/>
 </div>
 
+
+
 <body style="cursor: auto;">
 
-<div id="draggable" class="ui-widget-content ui-draggable ui-draggable-handle" style="position: relative; left: 673px; top: 280px;">
-	<div style = "height: 60px; width: 60px; position: relative; ">
-		<img src= 'assets/hexBorder.png' alt="" style = "height: 75px; width: 75px; position: absolute;" ondblclick = 'myfunc(resizable)'/>
-		<img src= 'http://google.com/favicon.ico' alt="" style="height: 42px; width: 42px; position:absolute; left:16px; top:5px" ondblclick = 'myfunc(resizable)'/>
-		<input type="text" style ="font-size: 10px; width:75px; position:absolute; top:50; background:transparent; border:transparent; color:white; text-align:center" maxlength = "6">
-	</div>
-	<div id="resizable" style="display:none;">
-		<iframe id="myiFrame" src="http://example.com" style="width:100%; height:100%	;"></iframe>
-		<div id="mask"></div>
-	</div>
-</div>
 
 
 <div id="postIt" style="position:absolute; left: 300px; top: 300px;">
@@ -220,8 +244,7 @@ function edDrop(id){
 	else $(id).hide();
 }
 
-</script>
-<script>
+
 var all_flakes = [];
 function myfunc(id){
 	window.console.log(id);
@@ -245,38 +268,55 @@ var original = document.getElementById('draggable');
 
 
 function duplicate() {
-    var clone = document.createElement('div');
 	var cloneID = "draggable" + ++i;
-   
-	
+	var clone = document.createElement("div");
+	clone.id= cloneID;
+	document.body.appendChild(clone);
+	window.console.log(clone.outerHTML);
+//	clone.outerHTML = "<div id='" + cloneID + "' class= 'ui-widget-content ui-draggable ui-draggable-handle' style='position: relative; left: 673px; top: 280px; height: 60px; width: 60px; ; padding: 0.5em; background:transparent; border:transparent;'> </div>"
 	var newURL = document.getElementById('textbox').value;
+	window.console.log(newURL);
 	if(newURL == '')
 		newURL = 'example.com';
 	if(newURL.substring(0,4) != 'http'){
 		newURL = 'http://' + newURL;
 	}
+	window.console.log(newURL);
 	
 	var test = new Image();
 	test.src = "http://" + newURL.split('/')[2].concat("/favicon.ico");
+	window.console.log(test.src);
+	/* //i think this broke favicon
 	if(!test.complete || test.naturalHeight == 0)
-		test.src = 'http://google.com/favicon.ico';
-	
-	var inner = "<div id='" + cloneID + "' class= 'ui-widget-content ui-draggable ui-draggable-handle' style='position: relative; left: 673px; top: 280px; height: 60px; width: 60px; ; padding: 0.5em; background:transparent; border:transparent;'> "
-				+ "<div style = 'height: 60px; width: 60px; position: relative; '> "
-				+ "<img src= 'assets/hexBorder.png' alt='' style = 'height: 75px; width: 75px; position: absolute;' ondblclick = 'myfunc(resizable" + i + ")'/>"
-				+ "<img src= '" + test.src + "' alt='' style='height: 42px; width: 42px; position:absolute; left:16px; top:5px;' ondblclick = 'myfunc(resizable" + i + ")'/></div>"
-				+ "<input type='text' style ='font-size: 10px; width:75px; position:absolute; top:57; background:transparent; border:transparent; color:white; text-align:center' maxlength = '6'>"
+		test.src = 'http://google.com/favicon.ico';*/
+	window.console.log(test.src);
+	var inner = "<div id='" + cloneID + "' class= 'ui-widget-content ui-draggable ui-draggable-handle' style='position: relative; left: 673px; top: 280px; height: 60px; width: 60px; ; padding: 0.5em; background:transparent; border:transparent;' ondblclick = 'myfunc(resizable" + i + ")'> "
+				+ "<li class='hex'></li>"
+				+ "<span class='innerHex'>"
+				+ "<img src= '" + test.src + "' alt='' style='height: 42px; width: 42px; position:absolute; top:10px; left: 12px;'/>"
+				+ "<input id = 'text" +  i + "'type='text' style='font-size: 15px; width:75px; color:black; text-align:center; background: transparent; border: transparent; position:absolute; top: 55px; left: -6px;' maxlength = '6'>"
+				+ "</span>"
 				+ " <div id='resizable" + i + "' style='width: 310px; height: 160px; padding: 0.5em; display:none;'> <iframe id='" 
 				+ "myiFrame" + i
 				+  "' src='http://example.com' style='width:100%; height:100%;'></iframe> <div id='mask" + i
 				+ "'></div> </div>"
+				+ "<li class='hex' style='width: calc(.85 * 4em); height: calc(.85 * 6.8em); left: -3px; top: -91px; z-index: -1; background: transparent;'></li>"
 				+ "</div>";
-	clone.innerHTML = inner;
+	clone.outerHTML = inner;
+	/*if(highlightBox == true){
+//		window.console.log(newTexID);
+		document.getElementById(cloneID).getElementsByTagName("INPUT")[0].style.border = "7px solid green";
+		document.getElementById(cloneID).getElementsByTagName("LI")[1].style.background = "#DA70D6";
+	}
+	else if(highlightDrag == true){
+		document.getElementById(cloneID).getElementsByTagName("LI")[1].style.background = "#DA70D6";
+	}
+	*/
 	
 //	window.console.log(inner);
 //	window.console.log(document.getElementById("myiFrame" + i));
 //	window.console.log(document.getElementById(clone.id).innerHTML);
-	document.body.appendChild(clone);
+//	document.body.appendChild(clone);
 	document.getElementById("myiFrame" + i).src = newURL;
 	
 	$(function() {
@@ -286,63 +326,20 @@ function duplicate() {
 			var element = document.getElementById($(this).attr("id"));
 			if(testDelete == true) {
 				element.parentNode.removeChild(element);
+				console.log("BEFORE");				
+				console.log(all_flakes);				
+				for(var x = 0; x < all_flakes.length; x++){
+					if(all_flakes[x].id == cloneID){
+						all_flakes.splice(x, 1);
+						console.log(all_flakes[x].id);
+					}
+				}
+				console.log("AFTER");				
+				console.log(all_flakes);				
 			}
 		});
 	} );
 	
-
-	$(function(){
-		$("#resizable".concat(i)).resizable({
-			start: function(event, ui) { 
-			   $("#mask".concat(i)).css("display","block");
-			},
-			stop: function(event, ui) {
-			   $("#mask".concat(i)).css("display","none");
-			}
-		});
-	});
-	
-	all_flakes.append(clone)
-}
-
-
-function duplicate1(x, y, url_, is_open, _label) {
-    var clone = original.cloneNode(true); // "deep" clone
-
-    clone.id = "draggable" + ++i; // there can only be one element with an ID
-    clone.style = "position: relative; left:" + x + "px; top: " + y + "px;";
-    original.parentNode.appendChild(clone);  
-		
-	var inner = "<div id='" + clone.id + "' class= 'ui-widget-content ui-draggable ui-draggable-handle' style='" + clone.style + "' height: 60px; width: 60px; ; padding: 0.5em; background:transparent; border:transparent;'> "
-				+ "<div style = 'height: 60px; width: 60px; position: relative; '> "
-				+ "<img src= 'assets/hexBorder.png' alt='' style = 'height: 75px; width: 75px; position: absolute;' ondblclick = 'myfunc(resizable" + i + ")'/>"
-				+ "<img src= '" + url_ + "' alt='' style='height: 42px; width: 42px; position:absolute; left:16px; top:5px;' ondblclick = 'myfunc(resizable" + i + ")'/></div>"
-				+ "<input type='text' style ='font-size: 10px; width:75px; position:absolute; top:57; background:transparent; border:transparent; color:white; text-align:center' maxlength = '6'>"
-				+ " <div id='resizable" + i + "' style='width: 310px; height: 160px; padding: 0.5em; display:none;'> <iframe id='" 
-				+ "myiFrame" + i
-				+  "' src='http://example.com' style='width:100%; height:100%;'></iframe> <div id='mask" + i
-				+ "'></div> </div>"
-				+ "</div>";
-	document.getElementById(clone.id).innerHTML = inner;
-	
-	document.getElementById("myiFrame" + i).src = url_;
-	clone.src = url_;
-	console.log(is_open);
-	if(!is_open){$(document.getElementById("myiFrame" + i)).hide();}
-	clone.label = _label;
-	
-	$(function() {
-		$("#".concat(cloneID)).draggable({iframeFix: true});
-		$("#".concat(cloneID)).on('click', function(){
-			window.console.log($(this).attr("id"));
-			var element = document.getElementById($(this).attr("id"));
-			if(testDelete == true) {
-				element.parentNode.removeChild(element);
-			}
-		});
-	} );
-	
-
 	$(function(){
 		$("#resizable".concat(i)).resizable({
 			start: function(event, ui) { 
@@ -354,23 +351,121 @@ function duplicate1(x, y, url_, is_open, _label) {
 		});
 	});
 	all_flakes.push(clone);
+   	window.console.log(clone);
+}
+
+
+
+function duplicate1(x, y, url_, is_open, _label) {
+	var cloneID = "draggable" + ++i;
+	var clone = document.createElement("div");
+	clone.id= cloneID;
+	document.body.appendChild(clone);
+	window.console.log(clone.outerHTML);
+//	clone.outerHTML = "<div id='" + cloneID + "' class= 'ui-widget-content ui-draggable ui-draggable-handle' style='position: relative; left: 673px; top: 280px; height: 60px; width: 60px; ; padding: 0.5em; background:transparent; border:transparent;'> </div>"
+	var newURL = url_
+	window.console.log(newURL);
+	if(newURL == '')
+		newURL = 'example.com';
+	if(newURL.substring(0,4) != 'http'){
+		newURL = 'http://' + newURL;
+	}
+	window.console.log(newURL);
+	
+	var test = new Image();
+	test.src = "http://" + newURL.split('/')[2].concat("/favicon.ico");
+	window.console.log(test.src);
+	/* //i think this broke favicon
+	if(!test.complete || test.naturalHeight == 0)
+		test.src = 'http://google.com/favicon.ico';*/
+	window.console.log(test.src);
+	var inner = "<div id='" + cloneID + "' class= 'ui-widget-content ui-draggable ui-draggable-handle' style='position: relative; left: " + x + "px; top: " + y + "px; height: 60px; width: 60px; ; padding: 0.5em; background:transparent; border:transparent;' ondblclick = 'myfunc(resizable" + i + ")'> "
+				+ "<li class='hex'></li>"
+				+ "<span class='innerHex'>"
+				+ "<img src= '" + test.src + "' alt='' style='height: 42px; width: 42px; position:absolute; top:10px; left: 12px;'/>"
+				+ "<input id = 'text" +  i + "'type='text' value ='" + _label + "'style='font-size: 15px; width:75px; color:black; text-align:center; background: transparent; border: transparent; position:absolute; top: 55px; left: -6px;' maxlength = '6'>"
+				+ "</span>"
+				+ " <div id='resizable" + i + "' style='width: 310px; height: 160px; padding: 0.5em; display:none;'> <iframe id='" 
+				+ "myiFrame" + i
+				+  "' src='http://example.com' style='width:100%; height:100%;'></iframe> <div id='mask" + i
+				+ "'></div> </div>"
+				+ "<li class='hex' style='width: calc(.85 * 4em); height: calc(.85 * 6.8em); left: -3px; top: -91px; z-index: -1; background: transparent;'></li>"
+				+ "</div>";
+	clone.outerHTML = inner;
+	/*
+	if(highlightBox == true){
+		window.console.log(newTexID);
+		document.getElementById(cloneID).getElementsByTagName("INPUT")[0].style.border = "7px solid green";
+		document.getElementById(cloneID).getElementsByTagName("LI")[1].style.background = "#DA70D6";
+	}
+	else if(highlightDrag == true){
+		document.getElementById(cloneID).getElementsByTagName("LI")[1].style.background = "#DA70D6";
+	}
+	*/
+	
+//	window.console.log(inner);
+//	window.console.log(document.getElementById("myiFrame" + i));
+//	window.console.log(document.getElementById(clone.id).innerHTML);
+//	document.body.appendChild(clone);
+	document.getElementById("myiFrame" + i).src = newURL;
+	
+	$(function() {
+		$("#".concat(cloneID)).draggable({iframeFix: true});
+		$("#".concat(cloneID)).on('click', function(){
+			window.console.log($(this).attr("id"));
+			var element = document.getElementById($(this).attr("id"));
+			if(testDelete == true) {
+				element.parentNode.removeChild(element);
+				console.log("BEFORE");				
+				console.log(all_flakes);				
+				for(var x = 0; x < all_flakes.length; x++){
+					if(all_flakes[x].id == cloneID){
+						all_flakes.splice(x, 1);
+						console.log(all_flakes[x].id);
+					}
+				}
+				console.log("AFTER");				
+				console.log(all_flakes);	
+			}
+		});
+	} );
+	
+	$(function(){
+		$("#resizable".concat(i)).resizable({
+			start: function(event, ui) { 
+			   $("#mask".concat(i)).css("display","block");
+			},
+			stop: function(event, ui) {
+			   $("#mask".concat(i)).css("display","none");
+			}
+		});
+	});
+	all_flakes.push(clone);
+   	window.console.log(clone);
 }
 
 function save() {
 	var save_string = "";
-	
+
 	for(var i = 0; i < all_flakes.length; i++){
 		c_id = all_flakes[i].id;
-		this_url = all_flakes[i].src + ',';
+		console.log("cid = " + c_id.substring(9));
+		var curr_url = document.getElementById("myiFrame" + c_id.substring(9));
+		var curr_label = document.getElementById("text" + c_id.substring(9));
+		f_label = curr_label.value; 
+		
+		this_url = curr_url.src + ',';
 		console.log(this_url);
-		curr_flake = document.getElementById(c_id);
+	
+		var curr_flake = document.getElementById(c_id);
+		console.log(curr_flake);
+		console.log("cid after url = " + c_id);
 		x_pos_ = curr_flake.style.left;// + ',';
 		x_pos = x_pos_.substring(0, x_pos_.length - 2) + ',';
 		console.log(x_pos);
 		console.log(x_pos);
 		y_pos_ = curr_flake.style.top;// + ',';
 		y_pos = y_pos_.substring(0, y_pos_.length - 2) + ',';
-		f_label = c_id.lable; 
 		console.log(username);
 		if (username === undefined || username === null) {}
 		else {
@@ -464,7 +559,7 @@ session_start();
 
 if(!(isset($_SESSION['username']))){
 	echo "You must be logged in";
-	echo "<head><meta HTTP-EQUIV='refresh' content='2; url=dan.php'></head>";
+	echo "<head><meta HTTP-EQUIV='refresh' content='2; url=../authenticate.php'></head>";
 }
 
 require_once '../login.php';
@@ -482,8 +577,9 @@ $query = "SELECT * FROM flakes WHERE user = '$username'";
 $result = mysqli_query($db_server, $query);
 if (!$result) die ("Result didn't work:" . mysqli_error($db_server));
 while ($row = $result->fetch_assoc()) {
-   $flakes[] = $row;
+   $flakes [] = $row;
 }
+if(is_array($flakes) || is_object($flakes)){
 foreach ($flakes as &$i){
 ?>
 	<script>
@@ -494,9 +590,11 @@ foreach ($flakes as &$i){
 	var _url_ = "<?php echo $i['url']; ?>";
 	params.push(_url_);
 	params.push(<?php echo $i['is_open']; ?>);
-	duplicate1(params[0], params[1], params[2], params[3]);
+	params.push("<?php echo $i['label']; ?>");
+	duplicate1(params[0], params[1], params[2], params[3], params[4]);
 	</script>
 <?php
+}
 }
 
 ?>
